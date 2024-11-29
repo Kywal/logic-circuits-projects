bec9e37ab8395acff4cdd49e0ab9ff973157b7f9
Ian Gabriel <ianeigor12@gmail.com>
2024-11-28 23:53:08 -0300

    finishing coffee machine

--- unidade-2/coffee-machine/coffee-machine.vhd
+++ unidade-2/coffee-machine/coffee-machine.vhd
@@ -2,24 +2,37 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY coffeeMachine IS PORT (
-	s: IN STD_LOGIC_VECTOR(2 downto 0);
+	s: IN STD_LOGIC_VECTOR(0 to 7);
 	o: OUT STD_LOGIC_VECTOR(0 to 7)
 	);
 END coffeeMachine;
 
 ARCHITECTURE arq OF coffeeMachine IS
-
+	COMPONENT codificador PORT (
+		s: IN STD_LOGIC_VECTOR(0 to 7);
+		o: OUT STD_LOGIC_VECTOR(2 downto 0)
+		);
+	END COMPONENT;
+	
+	COMPONENT decodificador  PORT (
+		s: IN STD_LOGIC_VECTOR(2 downto 0);
+		o: OUT STD_LOGIC_VECTOR(0 to 7)
+		);
+	END COMPONENT;
+	
+	COMPONENT verificador PORT (
+		s: IN STD_LOGIC_VECTOR(0 to 7);
+		o: OUT STD_LOGIC
+		);
+	END COMPONENT;
+		
+	
+	SIGNAL f: STD_LOGIC_VECTOR(2 downto 0);
+	SIGNAL f2: STD_LOGIC_VECTOR(0 to 7);
+	SIGNAL valido: STD_LOGIC;
 BEGIN
-	WITH s SELECT
-		o <= 
-			"10000000" WHEN "000",
-			"01000000" WHEN "001",
-			"00100000" WHEN "010",
-			"00010000" WHEN "011",
-			"00001000" WHEN "100",
-			"00000100" WHEN "101",
-			"00000010" WHEN "110",
-			"00000001" WHEN "111";
-			
-			
+	ver: verificador PORT MAP (s, valido);
+	cod: codificador PORT MAP (s, f);
+	decod: decodificador PORT MAP (f, f2);
+	o <= f2 WHEN valido = '1' ELSE "00000000";
 END arq;
---
+++ unidade-2/coffee-machine/decodificador.vhd
@@ -0,0 +1,25 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+ENTITY decodificador IS PORT (
+	s: IN STD_LOGIC_VECTOR(2 downto 0);
+	o: OUT STD_LOGIC_VECTOR(0 to 7)
+	);
+END decodificador;
+
+ARCHITECTURE arq OF decodificador IS
+
+BEGIN
+	WITH s SELECT
+		o <= 
+			"10000000" WHEN "000",
+			"01000000" WHEN "001",
+			"00100000" WHEN "010",
+			"00010000" WHEN "011",
+			"00001000" WHEN "100",
+			"00000100" WHEN "101",
+			"00000010" WHEN "110",
+			"00000001" WHEN "111";
+			
+			
+END arq;
